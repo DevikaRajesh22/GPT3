ab8f795811aa9fecb692081ee2c8e279676e37f3
DevikaRajesh22 <devikaraj699@gmail.com>
2024-01-04 14:47:04 +0530

    Base structure created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Footer, Blog, Possibilities, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+import './App.css';
 
 const App = () => {
     return (
-        <div>
-            <h1>GPT-3</h1>
+        <div className='App'>
+            <div className='gradient__bg'>
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibilities />
+            <CTA />
+            <Blog />
+            <Footer />
         </div>
     )
 }
