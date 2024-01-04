5313f9a212c7642a16fc54d2c44aa469c07c4b43
DevikaRajesh22 <devikaraj699@gmail.com>
2024-01-04 13:57:16 +0530

    container base files added

---
+++ src/containers/possibilities/Possibilities.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './possibilities.css';
+
+const Possibilities = () => {
+  return (
+    <div>
+      Possibilities
+    </div>
+  )
+}
+
+export default Possibilities;
