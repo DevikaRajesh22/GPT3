271925cf0984cc0bd27d4bf8a51a5ac79ba50925
DevikaRajesh22 <devikaraj699@gmail.com>
2024-01-05 13:37:20 +0530

    Feature secion completed designing

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './whatGPT3.css';
+import Features from '../features/Features';
 
 const WhatGPT3 = () => {
   return (
-    <div>
-      WhatGPT3
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+      <div className='gpt3__whatgpt3-features'>
+        <Features title='What is GPT-3?' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.'/>
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient__text'>The possibilities are boyond your imagination</h1>
+        <p>Explore the library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+        <Features title='Chatbots' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. '/>
+        <Features title='Knowledge base' text='At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments'/>
+        <Features title='Education' text='At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments'/>
+      </div>
     </div>
   )
 }
